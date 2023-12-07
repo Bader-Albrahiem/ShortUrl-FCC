9388f33d081852efb2182fbf9e08eff2a2f477f2
bader-albrahiem <balbrahiem@outlook.com>
2023-12-07 22:10:45 +0300

    Inital

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 app.use(cors({ optionsSuccessStatus: 200 }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/View/index.html");
 });
@@ -13,16 +13,14 @@ var urlDb = [];
 var nextUrl = 1;
 app.post("/api/shorturl", (req, res) => {
   const { url } = req.body;
-
+  console.log(url);
   if (!url) {
-    return res.status(400).json({ error: "Missing 'url' in the request body" });
+    return res.json({ error: "Missing 'url' in the request body" });
   }
   const shorturl = nextUrl++;
   if (url.startsWith("https://")) {
-    // Replace 'http://' with 'https://'
     urlDb.push({ original_url: url, short_url: shorturl });
   } else if (!url.includes("://")) {
-    // If the URL doesn't have any protocol, add 'https://'
     res.json({ error: "invalid url" });
   }
 
@@ -31,12 +29,13 @@ app.post("/api/shorturl", (req, res) => {
 
 app.get("/api/shorturl/:short_url", (req, res) => {
   const { short_url } = req.params;
+
   var checkUrl = urlDb.find((item) => {
     return item.short_url === parseInt(short_url);
   });
 
   if (!checkUrl || checkUrl.length < 1) {
-    res.json({ error: "No URL found for the given short URL" });
+    res.status(400).json({ error: "No URL found for the given short URL" });
   } else {
     res.redirect(checkUrl.original_url);
   }
